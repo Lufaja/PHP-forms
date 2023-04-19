75ce2f6cb3d7b34a0002da308961f9b09c7fda98
Lukas van Hulst <Lukas.v.hulst@gmail.com>
2023-04-19 10:05:42 +0200

    test

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="form-block">
+        <form  method="post" action="welcome2.php">
+            <span>* required field</span><br><br>
+            <label for="name">Name:<span> </span></label></br>
+            <input type="text" name="name"></br>
+            <label for="mail">E-mail:<span></span></label></br>
+            <input type="email" name="email"></br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    
+</body>
+</html>
