0ca23d0f7c1f4b9e7d9dc1a5e950ac7fc61f61d8
Lukas van Hulst <Lukas.v.hulst@gmail.com>
2023-05-08 10:06:46 +0200

    Onderdeel 2 gemaakt

--- form.php
+++ form.php
@@ -1,23 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="form-block">
-        <form  method="post" action="welcome2.php">
-            <span>* required field</span><br><br>
-            <label for="name">Name:<span> </span></label></br>
-            <input type="text" name="name"></br>
-            <label for="mail">E-mail:<span></span></label></br>
-            <input type="email" name="email"></br>
-            <input type="submit" value="Submit">
-        </form>
-    </div>
-    
+<?php
+$fname_message = $email_message = "";
+$fname_error = $email_error = "";
+
+$display_form = True;//turn form on
+if(isset($_POST['SubmitButton'])){ //check if form was submitted
+    $display_form = False;//if submit turn form off
+    echo "<h1>Your info is:</h1><br>";
+    $fname_input = $_POST['fname']; //get input text
+if (!preg_match("/^[a-zA-Z-' ]*$/",$fname_input)) {
+    $fname_error = "Only letters and white space allowed <br>";
+    echo $fname_error;
+}else{
+    $fname_message = "Your name is: ".$fname_input."<br>";
+    echo $fname_message;
+}
+
+  $email_input = $_POST['email']; //get input text
+if (!filter_var($email_input, FILTER_VALIDATE_EMAIL)) {
+    $email_error = "Invalid email format <br>";
+    echo $email_error;
+}else{
+    $email_message = "Your email is: ".$email_input."<br>";
+    echo $email_message;
+}
+?>
+<html>
+    <body>
+    <form>
+        <input type="button" value="Go back!" onclick="history.back()">
+    </form>
 </body>
 </html>
+<?php 
+
+}    
+if ($display_form){
+    ?>
+    <html>
+        <head><script>
+        function enableSubmit(){
+        let inputs = document.getElementsByClassName('required');
+        let btn = document.querySelector('input[type="submit"]');
+        let isValid = true;
+        for (var i = 0; i < inputs.length; i++){
+        let changedInput = inputs[i];
+        if (changedInput.value.trim() === "" || changedInput.value === null){
+        isValid = false;
+        break;
+        }
+        }
+        btn.disabled = !isValid;
+        }
+        </script></head>
+    <body>    
+    <form action="" method="post">
+
+        Name:<font color=red>*</font><input type="text" class="required" onkeyup="enableSubmit()" name="fname" /><br>
+        Email:<font color=red>*</font><input type="text" class="required" onkeyup="enableSubmit()" name="email"/><br>
+        <input type="submit" name="SubmitButton" disabled/>
+    </form>    
+    </body>
+    </html>
+<?php
+}
+?>
